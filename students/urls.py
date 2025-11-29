23f4758a87d19bf1cfc8b4cf36620b6c736cbea1
Sumant Kumar pandit <sumantpandit993127@gmail.com>
2025-11-29 19:17:46 +0530

    first commit

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,8 @@ from .views import student_home, student_edit, student_delete
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.student_home, name='student-home'),
+
+    # path('', views.student_home, name='student-home'),
     path('students/', views.student_list),
     path('students/<int:pk>/', views.student_detail),
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path('edit/<int:pk>/', student_edit, name='student-edit'),
     path('delete/<int:pk>/', student_delete, name='student-delete'),
 
-     path('login/', LoginView.as_view(template_name='students/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='students/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    
 ]
